9040150133d0ffcf708b82a20ed690d43cdc6f25
Nguyen Anh Quan <nanhquan143@gmail.com>
2025-05-02 22:59:19 +0700

    init app

--- hotel_booking_app/lib/pages/auth_gate.dart
+++ hotel_booking_app/lib/pages/auth_gate.dart
@@ -2,8 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
-import 'package:hotel_booking_app/main.dart';
-
 import 'home.dart';
 
 class AuthGate extends StatelessWidget {
